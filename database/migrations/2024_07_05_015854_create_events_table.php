392e0318ebfdbef078b59802c755a3cf3a51f9f6
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-07-05 13:43:26 +0700

    add-field-thumbnails

--- database/migrations/2024_07_05_015854_create_events_table.php
+++ database/migrations/2024_07_05_015854_create_events_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('thumbnail');
             $table->date('start_date');
             $table->date('end_date')->nullable();
             $table->string('location')->nullable();
