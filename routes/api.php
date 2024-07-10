637370af48ab5aa5ee9a8714ea8afa6943b0fe7c
Rio Ragil Ramdani <rioragilramdani7@gamail.com>
2024-07-10 22:08:03 +0700

    cek-

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,3 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\NewsController;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-Route::apiResource('categorys', CategoryController::class);
-Route::apiResource('articles', ArticleController::class);
-Route::apiResource('news', NewsController::class);
