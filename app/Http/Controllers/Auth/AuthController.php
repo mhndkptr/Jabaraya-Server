127a9f03c7b8b8afadb3b6bf2a424a04a692e4c3
mhndkptr <muhamadhendika77@gmail.com>
2024-07-10 21:24:08 +0700

    Add Auth and CRUD Profile

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        try 
+        {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users|email:dns',
+                'password' => 'required|string|min:8',
+                'password_confirmation' => 'required|string|min:8|same:password',
+                'avatar' => 'nullable|string',
+                'phone' => 'nullable|string',
+            ]);
+    
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'statusCode' => 401,
+                    'message' => 'validation error',
+                    'errors' => $validator->errors()
+                ], 401);
+            }
+    
+            if ($request->avatar) {
+                if ($request->avatar != 'https://firebasestorage.googleapis.com/v0/b/jabaraya-test.appspot.com/o/avatars%2Fdefault-avatar-1.png?alt=media' && $request->avatar != 'https://firebasestorage.googleapis.com/v0/b/jabaraya-test.appspot.com/o/avatars%2Fdefault-avatar-2.png?alt=media') {
+                    return response()->json([
+                        'status' => false,
+                        'statusCode' => 401,
+                        'message' => 'Avatar url is not valid',
+                    ], 401);
+                }
+
+                $avatarUrl = $request->avatar;
+            } else {
+                $avatarUrl = 'https://firebasestorage.googleapis.com/v0/b/jabaraya-test.appspot.com/o/avatars%2Fdefault-avatar-null.png?alt=media';
+            }
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'avatar' => $avatarUrl,
+                'phone' => $request->phone,
+            ]);
+    
+            $token = $user->createToken('USER_PERSONAL_TOKEN')->accessToken;
+    
+            return response()->json([
+                'status' => true,
+                'statusCode' => 200,
+                'message' => 'User created successfully',
+                'data' => [
+                    'token' => $token,
+                    'user' => $user,
+                    'social_auth' => ($user->providerAuths()->where('user_id', $user->id)->exists() ? $user->providerAuths()->where('user_id', $user->id)->first()->provider_name : null),
+                ],
+            ], 200);
+        } catch(\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'statusCode' => 500,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        try
+        {
+            $credentials = $request->only('email', 'password');
+            
+            if (Auth::attempt($credentials)) {
+                $user = Auth::user();
+                $token = $user->createToken('USER_PERSONAL_TOKEN')->accessToken;
+    
+                return response()->json([
+                    'status' => true,
+                    'statusCode' => 200,
+                    'message' => 'User login successfully',
+                    'data' => [
+                        'token' => $token,
+                        'user' => $user,
+                        'social_auth' => ($user->providerAuths()->where('user_id', $user->id)->exists() ? $user->providerAuths()->where('user_id', $user->id)->first()->provider_name : null),
+                    ],
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'statusCode' => 401,
+                    'message' => 'Email & password does not match',
+                ], 401);
+            }
+        } catch(\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'statusCode' => 500,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        try
+        {
+            $request->user()->token()->revoke();
+            return response()->json([
+                'status' => true,
+                'statusCode' => 200,
+                'message' => 'User successfully logged out',
+                'data' => []
+            ], 200);
+        } catch(\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'statusCode' => 500,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function tokenUpdate()
+    {
+        try 
+        {
+            $user = Auth::user();
+            $user->token()->revoke();
+            $token = $user->createToken('USER_PERSONAL_TOKEN')->accessToken;
+            return response()->json([
+                'status' => true,
+                'statusCode' => 200,
+                'message' => 'User verified',
+                'data' => [
+                    'token' => $token,
+                    'role' => $user->role
+                ]
+            ], 200);
+        } catch(\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'statusCode' => 500,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function unauthenticated()
+    {
+        return response()->json([
+            "status" => false,
+            'statusCode' => 401,
+            "message" => "Unauthenticated. Please login first",
+        ], 401);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,26 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Auth\SocialAuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Auth Routes
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::middleware('auth:api')->post('auth/logout', [AuthController::class, 'logout']);
+Route::middleware('auth:api')->post('auth/token-update', [AuthController::class, 'tokenUpdate']);
+Route::get('/unauthenticated', [AuthController::class, 'unauthenticated'])->name('login');
+
+// User Routes
+Route::middleware('auth:api')->group(function () {
+    Route::get('users', [UserController::class, 'index']);
+    Route::get('user', [UserController::class, 'show']);
+    Route::put('user', [UserController::class, 'update']);
+    Route::post('user/change-password', [UserController::class, 'changePassword']);
+});
+
+// Social Routes
+Route::get('login/{provider}', [SocialAuthController::class, 'redirectToProvider']);
+Route::get('login/{provider}/callback', [SocialAuthController::class, 'handleProviderCallback']);
